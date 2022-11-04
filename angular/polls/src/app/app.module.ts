b8b2f53fc13db75b06329e65d7e726e6c6bd77c5
redman424242 <redman424242@gmail.com>
2022-11-04 16:38:49 +0100

    Problemi precedenti + problemi compnente prova

--- angular/polls/src/app/app.module.ts
+++ angular/polls/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProvaComponent } from './prova/prova.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProvaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
